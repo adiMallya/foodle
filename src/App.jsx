9f483f8dc906fdc36f162c4fd9a25efa62c27cc1
adiMallya <adimallya99@gmail.com>
2023-06-11 01:44:42 +0530

    chore : mockman setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import { Routes, Route } from "react-router-dom";
+import Mockman from "mockman-js";
+
 function App() {
   return (
     <>
       <h1>Twizzle - &quot;Create, Engage, Twizzle&quot;</h1>
+      <Routes>
+        <Route path="/mockman" element={<Mockman />} />
+      </Routes>
     </>
   );
 }
