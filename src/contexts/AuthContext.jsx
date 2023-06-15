0acff3476bcf0740c78fa43b73c061d3a679e6d1
adiMallya <adimallya99@gmail.com>
2023-06-16 01:35:58 +0530

    add : Auth context for auth state mngmt

---
+++ src/contexts/AuthContext.jsx
@@ -0,0 +1,17 @@
+import { createContext, useContext, useReducer } from "react";
+import { initialAuthState, authReducer } from "src/features/auth";
+
+const AuthContext = createContext({});
+
+const AuthProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(initialAuthState, authReducer);
+  return (
+    <AuthContext.Provider value={{ ...state, authDispatch: dispatch }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+const useAuthContext = () => useContext(AuthContext);
+
+export { AuthProvider, useAuthContext };
