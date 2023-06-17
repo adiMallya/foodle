978dd8697a51ccdded2078b37f030c008f4a463e
adiMallya <adimallya99@gmail.com>
2023-06-17 14:00:24 +0530

    styles : device unit fixed

--- src/styles/breakpoints.js
+++ src/styles/breakpoints.js
@@ -1,11 +1,11 @@
 const breakpoints = {
     sm: '512px',
-    md: '769px',
+    md: '768px',
     lg: '1024px',
-}
+};
 
 export const device = {
     sm: `(min-width: ${breakpoints.sm})`,
     md: `(min-width: ${breakpoints.md})`,
     lg: `(min-width: ${breakpoints.lg})`
-}
+};
