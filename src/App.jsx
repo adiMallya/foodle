ecb6e3dc35ddcfe3704547803ea6cf04a1142cc5
adiMallya <adimallya99@gmail.com>
2023-06-18 02:32:51 +0530

    add : auth + home pages added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import { Routes, Route } from "react-router-dom";
-import { GlobalStyles, theme } from "src/styles";
 import Mockman from "mockman-js";
 import { ThemeProvider } from "styled-components";
+import { Toaster } from "react-hot-toast";
+
+import { LoginForm, SignUpForm, PrivateRoute } from "src/features/auth";
+import { Home } from "src/pages";
+import { GlobalStyles, theme } from "src/styles";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
+      <Toaster position="top-center" reverseOrder={false} />
       <Routes>
+        <Route element={<PrivateRoute />}>
+          <Route path="/" element={<Home />} />
+        </Route>
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/signup" element={<SignUpForm />} />
         <Route path="/mockman" element={<Mockman />} />
       </Routes>
     </ThemeProvider>
