620f585b5915e86cf70d35705ad31cc7a7d52622
adiMallya <adimallya99@gmail.com>
2023-06-22 23:47:19 +0530

    add : user actions

---
+++ src/features/users/userReducer.js
@@ -0,0 +1,45 @@
+import { ACTIONS } from "src/utils";
+
+const intialUserState = {
+    users: [],
+    bookmarks: [],
+    searchValue: "",
+    searchResults: [],
+    isLoading: true,
+    error: null
+};
+
+const userReducer = (state, { type, payload }) => {
+    switch (type) {
+        case ACTIONS.SET_USERS:
+            return {
+                ...state,
+                users: [...payload],
+                isLoading: false
+            };
+        case ACTIONS.SET_BOOKMARKS:
+            return {
+                ...state,
+                bookmarks: [...payload],
+                isLoading: false
+            };
+        case ACTIONS.SEARCH:
+            return {
+                ...state,
+                searchValue: payload.toLowerCase()
+            };
+        case ACTIONS.SET_RESULTS:
+            return {
+                ...state,
+                searchResults: [...payload]
+            };
+        case ACTIONS.SET_LOADING:
+            return {
+                ...state,
+                isLoading: true
+            };
+        default: return state;
+    }
+};
+
+export { intialUserState, userReducer };
