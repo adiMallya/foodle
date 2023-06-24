97a2093ed2e1bf01e87fa263c52e077c6550e297
adiMallya <adimallya99@gmail.com>
2023-06-24 17:50:27 +0530

    refact : fetch all users

--- src/contexts/UserContext.jsx
+++ src/contexts/UserContext.jsx
@@ -1,10 +1,16 @@
+import { useEffect } from "react";
 import { createContext, useContext, useReducer } from "react";
-import { intialUserState, userReducer } from "src/features/users";
+import { intialUserState, userReducer, getAllUsers } from "src/features/users";
 
-const UserContext = createContext();
+const UserContext = createContext({});
 
 const UserProvider = ({ children }) => {
   const [state, dispatch] = useReducer(userReducer, intialUserState);
+
+  useEffect(() => {
+    getAllUsers(dispatch);
+  }, []);
+
   return (
     <UserContext.Provider value={{ ...state, userDispatch: dispatch }}>
       {children}
