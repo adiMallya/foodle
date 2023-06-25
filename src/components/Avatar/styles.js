fa8fbb1c3c3f1ba7358cfdc31d9e51fa0a3cb8d0
adiMallya <adimallya99@gmail.com>
2023-06-25 12:24:57 +0530

    styles : alignment + height fixed

--- src/components/Avatar/styles.js
+++ src/components/Avatar/styles.js
@@ -2,13 +2,19 @@ import { styled } from "styled-components";
 import { FLEX } from "src/styles";
 
 const AvatarWrapper = styled.span`
+    height: 2rem;
+    width: 3rem;
+    border-radius: 50%;
+    overflow: hidden;
+
+    background-color: ${({ theme }) => theme.color.primary};
     cursor: pointer;
 `;
 
 const AvatarImage = styled.img`
-    height: 2rem;
-    width: 2rem;
-    border-radius: 50%;
+    height: 100%;
+    width: 100%;
+    object-fit: cover;
     font-size: 0.8rem;
 `;
 
