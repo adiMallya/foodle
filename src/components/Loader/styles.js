47065450202ecd4c2ac40f60880f48ce691d9cae
adiMallya <adimallya99@gmail.com>
2023-06-30 11:48:08 +0530

    styles : Loader width + height

--- src/components/Loader/styles.js
+++ src/components/Loader/styles.js
@@ -3,8 +3,8 @@ import { styled } from "styled-components";
 export const LoaderWrapper = styled.div`
   background: ${({ theme }) => theme.bgColor.secondary};
   position: fixed;
-  width: 100vw;
-  height: 100vh;
+  width: 100%;
+  height: 100%;
   display: flex;
   align-items: center;
   z-index: ${({ theme }) => theme.z[7]};
