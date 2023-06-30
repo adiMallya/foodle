2886d70d3616efedeacccc17eff2ff38ea6a7d18
adiMallya <adimallya99@gmail.com>
2023-06-30 11:47:51 +0530

    refact : loading data post login

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 
 import { PageLayout, Loader } from "src/components";
 import { CreatePost, PostCard, Sort, getAllPosts } from "src/features/posts";
+import { getAllUsers } from "src/features/users";
 import { usePostContext, useAuthContext, useUserContext } from "src/contexts";
 import { sortByType } from "src/utils";
 
@@ -9,40 +10,47 @@ import * as S from "./styles";
 
 function Home() {
   const { authUser } = useAuthContext();
-  const { users } = useUserContext();
-  const { posts, isLoading, sortBy, postDispatch } = usePostContext();
+  const { users, userDispatch, isLoading: waitingForUsers } = useUserContext();
+  const {
+    posts,
+    sortBy,
+    postDispatch,
+    isLoading: waitingForPosts,
+  } = usePostContext();
+
+  useEffect(() => {
+    getAllUsers(userDispatch);
+    getAllPosts(postDispatch);
+  }, []);
 
   const currentUser = users?.find(
-    (user) => user.username === authUser?.username
+    (user) => user.username === authUser.username
   );
 
   const followingUsers = currentUser?.following;
 
-  const postsOfCurrentUser = posts?.filter(
-    (post) => post.username === currentUser?.username
+  const postsOfFollowingUsers = posts?.filter((post) =>
+    followingUsers.some(
+      (followingUser) => followingUser.username === post.username
+    )
   );
 
-  var postsOfFollowingUsers = posts?.filter((post) =>
-    followingUsers?.some(
-      (followingUser) => followingUser.username === post?.username
-    )
+  const postsOfCurrentUser = posts?.filter(
+    (post) => post?.username === currentUser?.username
   );
 
   const userFeed = [...postsOfFollowingUsers, ...postsOfCurrentUser];
 
   const sortedPosts = sortByType(userFeed, sortBy);
 
-  useEffect(() => {
-    getAllPosts(postDispatch);
-  }, [postDispatch]);
-
+  if (waitingForPosts || waitingForUsers) return <Loader fullScreen="true" />;
   return (
     <PageLayout>
       <S.Wrapper>
         <CreatePost />
         <Sort />
         <div>
-          {isLoading ? (
+          {waitingForPosts ? (
             <Loader />
           ) : sortedPosts?.length ? (
             [...sortedPosts]
