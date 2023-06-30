3730f7540fabbc40ecf681933f29a2ca910913d5
adiMallya <adimallya99@gmail.com>
2023-06-30 17:05:38 +0530

    refact: load data on app load

--- src/contexts/PostContext.jsx
+++ src/contexts/PostContext.jsx
@@ -1,10 +1,17 @@
-import { createContext, useContext, useReducer } from "react";
-import { initialPostState, postReducer } from "src/features/posts";
+import { createContext, useContext, useReducer, useEffect } from "react";
+import { initialPostState, postReducer, getAllPosts } from "src/features/posts";
+import { ACTIONS } from "src/utils";
 
 const PostContext = createContext({});
 
 const PostProvider = ({ children }) => {
   const [state, dispatch] = useReducer(postReducer, initialPostState);
+
+  useEffect(() => {
+    dispatch({ type: ACTIONS.SET_LOADING, payload: true });
+    getAllPosts(dispatch);
+  }, []);
+
   return (
     <PostContext.Provider value={{ ...state, postDispatch: dispatch }}>
       {children}
--- src/contexts/UserContext.jsx
+++ src/contexts/UserContext.jsx
@@ -1,10 +1,16 @@
-import { createContext, useContext, useReducer } from "react";
-import { intialUserState, userReducer } from "src/features/users";
+import { createContext, useContext, useReducer, useEffect } from "react";
+import { intialUserState, userReducer, getAllUsers } from "src/features/users";
+import { ACTIONS } from "src/utils";
 
 const UserContext = createContext({});
 
 const UserProvider = ({ children }) => {
   const [state, dispatch] = useReducer(userReducer, intialUserState);
+
+  useEffect(() => {
+    dispatch({ type: ACTIONS.SET_LOADING, payload: true });
+    getAllUsers(dispatch);
+  }, []);
 
   return (
     <UserContext.Provider value={{ ...state, userDispatch: dispatch }}>
