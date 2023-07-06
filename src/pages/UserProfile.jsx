588587779e8eb74b2f6e05085cc72bb2e0dfdcb1
adiMallya <adimallya99@gmail.com>
2023-07-06 13:01:53 +0530

    feat : display user posts in profile page

--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -5,46 +5,55 @@ import { useUserContext, usePostContext } from "src/contexts";
 import { Button, Icon } from "src/components/atoms";
 import { PageLayout, Loader } from "src/components";
 import { getAllUsers, UserDetails } from "src/features/users";
+import { getAllPosts, PostCard } from "src/features/posts";
 import * as S from "./styles";
 
 const UserProfile = () => {
   const { userName } = useParams();
   const navigate = useNavigate();
 
-  const { users, userDispatch, isLoading } = useUserContext();
-  const { posts } = usePostContext();
+  const { users, userDispatch } = useUserContext();
+  const { posts, postDispatch, isLoading } = usePostContext();
 
   useEffect(() => {
     getAllUsers(userDispatch);
-  }, [userName, users]);
+    getAllPosts(postDispatch);
+  }, [userName, users, posts]);
 
   const currentUser = users?.find((user) => user.username === userName);
   const currentUserPosts = posts?.filter((post) => post.username === userName);
 
   return (
     <PageLayout>
-      {isLoading ? (
-        <Loader />
-      ) : (
-        <S.Wrapper>
-          <S.Header align="left">
-            <Button
-              variant="icon"
-              aria-label="Back button"
-              type="button"
-              onClick={() => navigate(-1)}
-            >
-              <Icon icon={faArrowLeftLong} title="Back" />
-            </Button>
-            <div>
-              <h3>
-                {currentUser?.firstName + " " + currentUser?.lastName || ""}
-              </h3>
-            </div>
-          </S.Header>
-          {currentUser ? <UserDetails currentUser={currentUser} /> : null}
-        </S.Wrapper>
-      )}
+      <S.Wrapper>
+        <S.Header align="left">
+          <Button
+            variant="icon"
+            aria-label="Back button"
+            type="button"
+            onClick={() => navigate(-1)}
+          >
+            <Icon icon={faArrowLeftLong} title="Back" />
+          </Button>
+          <div>
+            <h3>
+              {currentUser?.firstName + " " + currentUser?.lastName || ""}
+            </h3>
+          </div>
+        </S.Header>
+        {currentUser ? <UserDetails currentUser={currentUser} /> : null}
+        {isLoading ? (
+          <Loader />
+        ) : currentUserPosts?.length ? (
+          [...currentUserPosts]
+            .reverse()
+            .map((post) => <PostCard key={post?._id} post={post} />)
+        ) : (
+          <S.NoData>
+            <p>No Posts</p>
+          </S.NoData>
+        )}
+      </S.Wrapper>
     </PageLayout>
   );
 };
